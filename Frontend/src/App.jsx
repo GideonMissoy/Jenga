0736322001a2e3667bb685b00e64a9672dc151e0
GideonMissoy <giddykips2030@gmail.com>
2024-05-17 00:48:01 +0300

    Updating pages and adding routes.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,14 @@ import react from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Home from "./pages/Home";
+import About from './pages/About';
+import Buying from './pages/Buying';
+import Contact from './pages/Contact';
+import Contractors from './pages/Contractors';
+import Index from "./pages/Index";
+import Profile from './pages/Profile';
+import Project from './pages/Project';
+import Projects from './pages/Projects';
 import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -21,16 +28,59 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route
-          path="/"
+          path="/contractors"
           element={
             <ProtectedRoute>
-              <Home />
+              <Contractors />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/buying"
+          element={
+            <ProtectedRoute>
+              <Buying />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/selling"
+          element={
+            <ProtectedRoute>
+              <Selling />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/projects"
+          element={
+            <ProtectedRoute>
+              <Projects />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/project/<int:pk>"
+          element={
+            <ProtectedRoute>
+              <Project />
             </ProtectedRoute>
           }
         />
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Index />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
