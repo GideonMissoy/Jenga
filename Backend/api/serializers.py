fa8731ab09750e0b48e135ee1bd266615c843508
GideonMissoy <giddykips2030@gmail.com>
2024-05-26 17:57:26 +0300

    Cleaning up.

--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -6,7 +6,6 @@ from .models import Project, Bid, User, Profile, Reviews
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
     phone_no = serializers.IntegerField(required=True)
-
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
     password = serializers.CharField(min_length=6, max_length=15, write_only=True)
@@ -35,7 +34,7 @@ class ReviewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reviews
         fields = '__all__'
-        
+
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
