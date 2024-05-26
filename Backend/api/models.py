ee9f2fe349d445944e6a89dfb1f0f6f6c7e5a7a0
GideonMissoy <giddykips2030@gmail.com>
2024-05-26 07:47:12 +0300

    Adding a custom user model and serializer.

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,6 +1,80 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils import timezone
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address.')
+        
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            )
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_staff(self, email, first_name, last_name, password=None):
+        user = self.create_staff(
+            email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+        )
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, first_name, last_name, password=None):
+        user = self.create_superuser(
+            email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+        )
+        user.is_Admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    username = None
+    first_name = models.CharField(max_length=75)
+    last_name = models.CharField(max_length=75)
+    email = models.EmailField(verbose_name='email_address', max_length=75, unique=True, null=False)
+    phone_no = models.IntegerField(verbose_name='phone number', null=False, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'phone_no'
+    REQUIRED_FIELDS = [first_name, last_name, email]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.is_staff
+
+    @property
+    def is_admin(self):
+        "Is the user a member of staff?"
+        return self.is_admin   
 
 
 class Project(models.Model):
@@ -21,3 +95,14 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Bid(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    proposal = models.TextField(blank=True)
+    bid_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Bid for {self.project.title} by {self.bidder.username}"
--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -1,15 +1,37 @@
-from django.contrib.auth.models import User
+from django.db.models.fields import SlugField
 from rest_framework import serializers
+from .models import Project, Bid, User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    phone_no = serializers.IntegerField(required=True)
+
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    password = serializers.CharField(min_length=6, max_length=15, write_only=True)
+
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
+        fields = ["email", "phone_no", "first_name", "last_name", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'user', 'title', 'budget', 'location', 'description', 'status', 'created_at', 'updated_at']
+
+
+class BidSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bid
+        field = ['id', 'project', 'bidder', 'bid_amount', 'proposal', 'bid_time']
