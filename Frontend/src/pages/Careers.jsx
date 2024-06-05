f5451f6c433371d11f880691d36c89af9bc1792b
MGK-Kagai <swifttask001@gmail.com>
2024-06-05 17:57:42 +0300

    Contact us page complete.

--- Frontend/src/pages/Careers.jsx
+++ Frontend/src/pages/Careers.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
+import { FaLocationDot } from 'react-icons/fa6';
+import { MdEmail } from 'react-icons/md';
+import { FaPhoneFlip } from 'react-icons/fa6';
 
 export default function Careers() {
   return (
@@ -16,20 +19,25 @@ export default function Careers() {
           </h2>
         </div>
         <div className='flex flex-col lg:flex-row my-5 lg:my-11'>
-          <div className='w-full lg:w-1/3 px-3 flex flex-col my-3 lg:mr-5 lg:my-0 lg:justify-center'>
+          <div className='w-full lg:w-1/2'>
+            <img
+              src='https://plus.unsplash.com/premium_photo-1661301167646-d28bbf327e65?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQ1fHxjb25zdHJ1Y3Rpb24lMjBzaXRlfGVufDB8fDB8fHww'
+              alt='Sample Photo'
+              className='rounded-3xl'
+            />
+          </div>
+          <div className='w-full lg:w-1/2 px-3 flex flex-col my-3 lg:mr-5 lg:my-0 lg:justify-center'>
             <h1 className='lg:text-5xl text-2xl font-bold'>
               Become a part of Jenga
             </h1>
             <p className='lg:my-3 lg:text-2xl font-bold text-grey-800'>
               Be part of the team that brings this vision to life
             </p>
-          </div>
-          <div className='w-full lg:w-2/3'>
-            <img
-              src='https://plus.unsplash.com/premium_photo-1661301167646-d28bbf327e65?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQ1fHxjb25zdHJ1Y3Rpb24lMjBzaXRlfGVufDB8fDB8fHww'
-              alt='Sample Photo'
-              className='rounded-3xl'
-            />
+            <div className='w-full flex justify-center'>
+              <button className='bg-green-500 hover:bg-green-700 text-slate-900 font-bold p-2 lg:px-5 lg:py-3 rounded-full'>
+                See open Positions
+              </button>
+            </div>
           </div>
         </div>
         <div className='mx-3 lg:mx-11 '>
@@ -40,51 +48,51 @@ export default function Careers() {
           <div className='flex flex-col lg:flex-row p-3 bg-slate-200'>
             <div className='w-full lg:w-1/2 lg:p-7'>
               <p>
-                Contact our HR team to find out the open positions in Jenga
-                right now. You will be provided with our contract roles and the
-                terms of service
+                Contact our HR team to find out more on the open positions at
+                Jenga right now. You will be provided with our contract roles
+                and the terms of service
               </p>
               <p>
                 Jenga Aims to foster a great conducive working environment for
                 its staff at all times.
               </p>
             </div>
-            <div className='w-full lg:w-1/2 lg:p-5 bg-slate-900 text-white'>
+            <div className='w-full lg:w-1/2 my-3 lg:p-5 bg-slate-900 text-white'>
               <div className='flex align-center text-center justify-between py-2'>
                 <div className='flex'>
-                  {/* <FaLocationDot size={25} className='text-pink-600' /> */}
+                  <FaLocationDot size={25} className='text-pink-600' />
                   <span className='ml-2 lg:ml-4 text-1xl lg:text-2xl'>
                     Address
                   </span>
                 </div>
                 <div>
-                  <span className='justify-end text-green-500 text-1xl lg:text-2xl'>
+                  <span className='justify-end text-green-500 pr-3 text-1xl lg:text-2xl'>
                     Nairobi, Kenya
                   </span>
                 </div>
               </div>
               <div className='flex align-center text-center justify-between py-2'>
                 <div className='flex'>
-                  {/* <MdEmail /> */}
+                  <MdEmail size={25} className='text-pink-600' />
                   <span className='ml-2 lg:ml-4 text-1xl lg:text-2xl'>
                     Email
                   </span>
                 </div>
                 <div>
-                  <span className='justify-end text-green-500 text-1xl lg:text-2xl'>
-                    gideon.missoy@gmail.com
+                  <span className='justify-end text-green-500 pr-3 text-1xl lg:text-2xl'>
+                    careers@jenga.com
                   </span>
                 </div>
               </div>
               <div className='flex align-center text-center  justify-between py-2'>
                 <div className='flex'>
-                  {/* <FaPhoneFlip size={25} className='text-pink-600' /> */}
+                  <FaPhoneFlip size={25} className='text-pink-600' />
                   <span className='ml-2 lg:ml-4 text-1xl lg:text-2xl'>
                     Phone Number
                   </span>
                 </div>
                 <div>
-                  <span className='justify-end text-green-500 text-1xl lg:text-2xl'>
+                  <span className='justify-end text-green-500 pr-3 text-1xl lg:text-2xl'>
                     +254704158777
                   </span>
                 </div>
--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -1,13 +1,119 @@
 import React from 'react';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
+import { FaLocationDot } from 'react-icons/fa6';
+import { MdEmail } from 'react-icons/md';
+import { FaPhoneFlip } from 'react-icons/fa6';
 
 function Contact() {
   return (
     <div>
       <Nav />
-      <h1>This page contains the COntact Us section</h1>
-      <p>It contains information for contacting the Staff at Jenga</p>
+      <div className='m-3 lg:m-11'>
+        <div className='flex flex-col items-center'>
+          <h1 className='text-2xl lg:text-6xl text-green-600 font-bold'>
+            Contact Us
+          </h1>
+          <p className='text-green-950 font-bold py-2 lg:py-5'>
+            Reach out to us for enquiries and support through the provided email
+            options.
+            <br /> You can also reach us through the landline number provided
+            during office hours.
+            <br /> Additionally, you can visit our office in the address
+            provided
+          </p>
+        </div>
+        <div className='my-5 lg:my-11 flex flex-col bg-slate-200 rounded-3xl lg:flex-row'>
+          <div className='w-full lg:w-1/2 flex my-5 justify-center'>
+            <table>
+              <tbody>
+                <tr className='flex flex-row my-3'>
+                  <td className='flex items-center px-3'>
+                    <FaLocationDot size={37} className='text-pink-600' />
+                  </td>
+                  <td className='flex items-center text-2xl font-bold pr-3 lg:px-5'>
+                    Address
+                  </td>
+                  <td>
+                    <h2 className='font-bold text-green-800 flex items-center'>
+                      Jenga Plaza, <br /> 2136 Haille Sellasie Avenue, <br />
+                      Nairobi, Kenya.
+                    </h2>
+                  </td>
+                </tr>
+                <tr className='flex flex-row'>
+                  <td className='flex items-center px-3'>
+                    <MdEmail size={37} className='text-pink-600' />
+                  </td>
+                  <td className='flex items-center text-2xl font-bold pr-3 lg:px-5'>
+                    Enquiries <br />
+                    Support
+                  </td>
+                  <td>
+                    <h2 className='font-bold text-green-800 flex items-center'>
+                      enquiries@jenga.com
+                      <br />
+                      support@jenga.com
+                    </h2>
+                  </td>
+                </tr>
+                <tr className='flex flex-row'>
+                  <td className='flex items-center px-3'>
+                    <FaPhoneFlip size={37} className='text-pink-600' />
+                  </td>
+                  <td className='flex items-center text-2xl font-bold pr-3 lg:px-5'>
+                    Landline <br />
+                    Mobile
+                  </td>
+                  <td>
+                    <h2 className='font-bold text-green-800 flex items-center'>
+                      +254712136452 <br />
+                      +254743611343
+                    </h2>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className='w-full lg:w-1/2 text-white bg-slate-900'>
+            <h1 className='text-2xl lg:text-5xl font-bold py-3 flex justify-center'>
+              Send Message
+            </h1>
+            <div className='px-4 lg:px-9 mt-2'>
+              <input
+                type='text'
+                placeholder='Full name'
+                className='bg-slate-900 border-none font-bold focus:outline-none focus:shadow-outline'
+              />
+              <hr className='text-white' />
+            </div>
+            <div className='px-4 lg:px-9 my-5'>
+              <input
+                type='text'
+                placeholder='Email'
+                className='bg-slate-900 border-none font-bold focus:outline-none focus:shadow-outline'
+              />
+              <hr className='text-white' />
+            </div>
+            <div className='px-4 lg:px-9 my-5'>
+              <input
+                type='text'
+                placeholder='Type your message...'
+                className='bg-slate-900 border-none font-bold focus:outline-none focus:shadow-outline'
+              />
+              <hr className='text-white' />
+            </div>
+            <div className='flex justify-center py-3'>
+              <button
+                type='button'
+                className='bg-green-700 font-bold px-3 py-1 lg:px-5 rounded-3xl hover:bg-green-600'
+              >
+                Send
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
